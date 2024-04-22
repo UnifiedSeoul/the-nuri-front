0c1601625f317edf8d466eaf86b40b3c84c4f9fc
YongHyunKing <yhkim8246@gmail.com>
2024-04-22 16:41:33 +0900

    feat: add login api

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 
+import { LoginAPI } from '../services/api';
+
 /* SVG */
 import { ReactComponent as FullLogo } from '../images/the-nuri-full-logo.svg';
 
@@ -10,6 +12,10 @@ const Login = () => {
 
   const handleUsernameChange = (e) => setUsername(e.target.value);
   const handlePasswordChange = (e) => setPassword(e.target.value);
+  const handleLogin = async () => {
+    const response = await LoginAPI(username, password);
+    console.log(response);
+  }
 
 
   return (
@@ -18,7 +24,7 @@ const Login = () => {
         <FullLogo style={{ width: '250px', height: '100px' }} />
         <input className="form-inputbox" type="text" value={username} onChange={handleUsernameChange} placeholder='아이디' />
         <input className="form-inputbox" type="password" value={password} onChange={handlePasswordChange} placeholder='비밀번호' />
-        <button className="login-button">로그인</button>
+        <button className="login-button" onClick={handleLogin}>로그인</button>
       </div>
     </div>
   )
