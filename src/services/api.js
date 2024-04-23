f1581a62b7702a61bf4b244f7fed903564f50584
YongHyunKing <yhkim8246@gmail.com>
2024-04-23 21:27:59 +0900

    feat: Send user date in URL-encoded format

--- src/services/api.js
+++ src/services/api.js
@@ -1,20 +1,19 @@
-// import axios from "axios";
-
-// const SERVER_URI = "http://localhost:5002"
+import axios from "axios";
+import qs from "qs";
+const SERVER_URI = "http://localhost:8080"
 
 export const LoginAPI = async (userId, password) => {
   const data = {
-    userId: userId,
+    username: userId,
     password: password
   }
-  return data;
-  // }
-  // try {
-  //   const response = await axios.post(SERVER_URI + '/api/login', data);
-  //   console.log(response.data);
-  //   return response.data;
-  // } catch (error) {
-  //   console.error('Error during login:', error);
-  //   throw error;
-  // }
+
+  try {
+    const response = await axios.post(SERVER_URI + '/login', qs.stringify(data));
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error('Error during login:', error);
+    throw error;
+  }
 }
